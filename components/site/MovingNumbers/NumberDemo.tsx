3b6267354c4a6f5e94e8dee9345a934bf1cfd9e6
it21294198 <it21294198@my.sliit.lk>
2024-11-19 00:00:18 +0530

    add easeout

--- components/site/MovingNumbers/NumberDemo.tsx
+++ components/site/MovingNumbers/NumberDemo.tsx
@@ -1,5 +1,6 @@
 import MotionNumber from 'motion-number';
 import React, { useState, useEffect } from 'react';
+import { easeOut } from 'framer-motion'
 
 export default function NumberDemo() {
   const [numberValue, setNumberValue] = useState(new Date().getSeconds());
@@ -20,6 +21,22 @@ export default function NumberDemo() {
         value={numberValue}
         format={{ notation: 'compact' }} // Intl.NumberFormat() options
         locales="en-US" // Intl.NumberFormat() locales
+      />
+      <hr/>
+      <MotionNumber
+        value={numberValue}
+        format={{ notation: 'compact' }} // Intl.NumberFormat() options
+        locales="en-US" // Intl.NumberFormat() locales
+        transition={{
+            // Applied to layout animations on individual characters:
+            layout: { type: 'spring', duration: 0.7, bounce: 0 },
+            // Used for the digit animations:
+            y: { type: 'spring', duration: 0.7, bounce: 0.25 },
+    
+            // Opacity applies to entering/exiting characters.
+            // Note the use of the times array, explained below:
+            opacity: { duration: 0.7, ease: easeOut, times: [0, 0.3] } // 0.3s perceptual duration
+        }}    
       />
     </div>
   );
